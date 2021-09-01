a87d694dbcbcdb4531c4bb965c77ce42cf7ed782
lucas simoes <lucas.simoes@synkar.com>
2021-09-01 00:25:41 -0300

    mudança nos parametros da rede e funçao de calculo da media dos individuos

--- es.py
+++ es.py
@@ -6,6 +6,9 @@ from evostra import EvolutionStrategy
 from feed_forward_network import FeedForwardNetwork
 import pickle
 
+import wandb
+import datetime
+
 import math
 import numpy
 import array
@@ -15,9 +18,9 @@ import multiprocessing
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Evolution Strategies. ')
-parser.add_argument('--env',default="Humanoid-v2")
-parser.add_argument('--render',type=bool,default=False)
+parser = argparse.ArgumentParser(description="Evolution Strategies. ")
+parser.add_argument("--env", default="Humanoid-v2")
+parser.add_argument("--render", type=bool, default=False)
 
 args = parser.parse_args()
 
@@ -33,21 +36,40 @@ def env_info(env_name):
     env.close()
     return observation_space, action_space
 
+
 observationSpace, actionSpace = env_info(args.env)
+
+wandb.init(project="Evolution-Estrategy", entity="lucas-simoes")
+config = wandb.config
+config.LAMBDA = 40
+config.MU = 1
+config.sigma = 0.3
+config.mutateAtribute = 0.1
+config.numThreads = 8
+config.numGen = 200
+config.mutatePop = 0.9
+config.layer_sizes = [observationSpace, 64, 64, actionSpace]
+config.env = args.env
+config.iterations = int(3e4)
+
 # A feed forward neural network with input size of 5, two hidden layers of size 4 and output of size 3
-layer_sizes = [observationSpace, 64,64, actionSpace]
-model = FeedForwardNetwork(layer_sizes = layer_sizes)
+layer_sizes = config.layer_sizes
+model = FeedForwardNetwork(layer_sizes=layer_sizes)
 
 
-IND_SIZE = math.prod(layer_sizes) #tamanho do individuo, sendo a quantidade de pesos (376*32*16*17)
+IND_SIZE = math.prod(
+    layer_sizes
+)  # tamanho do individuo, sendo a quantidade de pesos (376*32*16*17)
 MIN_VALUE = -1
 MAX_VALUE = 1
-MIN_STRATEGY = -0.2
-MAX_STRATEGY = 0.2
+MIN_STRATEGY = -0.5
+MAX_STRATEGY = 0.5
 max_step = 100000
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", array.array, typecode="d", fitness=creator.FitnessMax, strategy=None)
+creator.create(
+    "Individual", array.array, typecode="d", fitness=creator.FitnessMax, strategy=None
+)
 creator.create("Strategy", array.array, typecode="d")
 
 # Individual generator
@@ -55,6 +77,7 @@ def generateES(icls, scls, size, imin, imax, smin, smax):
     ind = icls(random.uniform(imin, imax) for _ in range(size))
     ind.strategy = scls(random.uniform(smin, smax) for _ in range(size))
     return ind
+
 
 def checkStrategy(minstrategy):
     def decorator(func):
@@ -65,54 +88,83 @@ def checkStrategy(minstrategy):
                     if s < minstrategy:
                         child.strategy[i] = minstrategy
             return children
+
         return wrappper
+
     return decorator
 
 
 toolbox = base.Toolbox()
-toolbox.register("individual", generateES, creator.Individual, creator.Strategy,
-    IND_SIZE, MIN_VALUE, MAX_VALUE, MIN_STRATEGY, MAX_STRATEGY)
+toolbox.register(
+    "individual",
+    generateES,
+    creator.Individual,
+    creator.Strategy,
+    IND_SIZE,
+    MIN_VALUE,
+    MAX_VALUE,
+    MIN_STRATEGY,
+    MAX_STRATEGY,
+)
 
-
-# def evalOneMax(individual):
-#     return sum(individual),
 
 def fitness(individual):
     reward = simulate(individual, False)
     return reward
 
 
+def selAverage(individuals, k, fit_attr="fitness"):
+    ind = individuals[0]
+    print("K = {}".format(k))
+    fitness = abs(getattr(ind, fit_attr).values[0])
+    for j in range(len(ind)):
+        ind[j] = ind[j] * fitness
+    sum_fitness = fitness
+
+    for i in range(1, len(individuals)):
+        fitness = abs(getattr(individuals[i], fit_attr).values[0])
+        for j in range(len(individuals[i])):
+            ind[j] += fitness * individuals[i][j]
+        sum_fitness += fitness
+
+    for j in range(len(ind)):
+        ind[j] /= sum_fitness
+    print("Mean Fitness: {}".format(sum_fitness / len(individuals)))
+    return [ind for _ in range(k)]
+
+
 def simulate(individual, render):
-    env = gym.make(args.env) #criando ambiente para cada indivíduo "pesos"
-    model.set_weights(individual)  #alterando os pesos da rede
+    env = gym.make(config.env)  # criando ambiente para cada indivíduo "pesos"
+    model.set_weights(individual)  # alterando os pesos da rede
     # here our best reward is zero
     reward = 0
     obs = env.reset()
     for step in range(max_step):
         if render:
             env.render()
-        #print(model.predict(obs))
-        #action = np.argmax(model.predict(obs)) # your agent here (this takes random actions) tirar duvida sobre o que a ação significa
         obs, rew, done, info = env.step(model.predict(obs))
-        reward+=rew     
+        reward += rew
 
         if done:
-            #env.reset_model()
-            #print(step)
-            break        
-    #print(step)
+            break
     env.close()
     return (reward,)
 
 
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("mate", tools.cxESBlend, alpha=0.1)
-#toolbox.register("mutate", tools.mutESLogNormal, c=1.0, indpb=0.03)
-toolbox.register("mutate", tools.mutGaussian,mu=0, sigma=0.1, indpb=0.1)
-toolbox.register("select", tools.selTournament, tournsize=3)
+# toolbox.register("mutate", tools.mutESLogNormal, c=1.0, indpb=0.03)
+toolbox.register(
+    "mutate",
+    tools.mutGaussian,
+    mu=0,
+    sigma=config.sigma,
+    indpb=config.mutateAtribute,
+)  # aumentar um pouco o ruido
+toolbox.register("select", selAverage)  # trocar pelo metodo roleta
 toolbox.register("evaluate", fitness)
 
-pool = multiprocessing.Pool(processes=8)
+pool = multiprocessing.Pool(processes=config.numThreads)
 toolbox.register("map", pool.map)
 
 toolbox.decorate("mate", checkStrategy(MAX_STRATEGY))
@@ -121,7 +173,7 @@ toolbox.decorate("mutate", checkStrategy(MAX_STRATEGY))
 
 def main():
     random.seed()
-    MU, LAMBDA = 2, 14 
+    MU, LAMBDA = config.MU, config.LAMBDA  # aumentar a população
     pop = toolbox.population(n=MU)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -129,16 +181,32 @@ def main():
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
-    
-    pop, logbook = algorithms.eaMuCommaLambda(pop, toolbox, mu=MU, lambda_=LAMBDA, 
-        cxpb=0.2, mutpb=0.7, ngen=1000, stats=stats, halloffame=hof)
 
-    with open(args.env +".pkl", 'wb') as fp:
+    pop, logbook = algorithms.eaMuCommaLambda(
+        pop,
+        toolbox,
+        mu=MU,
+        lambda_=LAMBDA,
+        cxpb=0,
+        mutpb=config.mutatePop,
+        ngen=config.numGen,
+        stats=stats,
+        halloffame=hof,
+    )
+
+    modelPath = (
+        "models/"
+        + str(datetime.datetime.now()).replace(" ", "_").replace(":", "-").split(".")[0]
+        + "_"
+        + config.env
+        + ".pkl"
+    )
+    with open(modelPath, "wb") as fp:
         sol = dict(population=pop, logbook=logbook, halloffame=hof)
         pickle.dump(sol, fp)
+        print("Saved " + modelPath)
     return pop, logbook, hof
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
--- teste.py
+++ teste.py
@@ -1,13 +1,14 @@
 from evostra.models import FeedForwardNetwork
 import pickle
 from environment import env_info
+
 from es import simulate
 import argparse
 import gym
 
-parser = argparse.ArgumentParser(description='Evolution Strategies.')
-parser.add_argument('--env',default="Humanoid-v2")
-parser.add_argument('--model',default=None)
+parser = argparse.ArgumentParser(description="Evolution Strategies.")
+parser.add_argument("--env", default="Humanoid-v2")
+parser.add_argument("--model", default=None)
 
 args = parser.parse_args()
 
@@ -15,34 +16,35 @@ observationSpace, actionSpace = env_info(args.env)
 
 
 # A feed forward neural network with input size of 5, two hidden layers of size 4 and output of size 3
-model = FeedForwardNetwork(layer_sizes=[observationSpace, 64,64, actionSpace])
+model = FeedForwardNetwork(layer_sizes=[observationSpace, 64, 64, actionSpace])
 
-with open(args.model,'rb') as fp:
-   model.set_weights(pickle.load(fp))
+
+with open(args.env + ".pkl", "rb") as fb:
+    weights = pickle.load(fb)
+
+ind = weights["halloffame"][0]
+
+model.set_weights(ind)
+# get_reward = simulate(ind, True)
+# print(get_reward)
+# while True:
+#     get_reward = simulate(ind, True)
+# with open(args.model,'rb') as fp:
+#   model.set_weights(pickle.load(fp))
 
 env = gym.make(args.env)
 obs = env.reset()
-reward = 0 
-while True:       
+reward = 0
+
+while True:
     env.render()
-    #print(model.predict(obs))
-    action = model.predict(obs) # your agent here (this takes random actions)
+    # print(model.predict(obs))
+    action = model.predict(obs)  # your agent here (this takes random actions)
     obs, rew, done, info = env.step(action)
-    reward+=rew
+    reward += rew
 
     if done:
-        #print(step)
+        # print(step)
         print(reward)
         env.reset()
-        reward = 0    
-       
-# with open(args.env+ ".pkl", 'rb') as fb:
-#     model = pickle.load(fb)
-
-# ind = model['halloffame'][0]
-
-
-# get_reward = simulate(ind, True)
-# print(get_reward)
-# while True:    
-#     get_reward = simulate(ind, True)
+        reward = 0
